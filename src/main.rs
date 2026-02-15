bea0be48aec119aa954adeffb5848ad035e92374
Farid Guzman <farid.guzman91@hotmail.com>
2026-02-15 23:01:07 +0100

    added specs, created world and storage regs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, RGB, Rltk};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB
+}
+
+
+impl Component for Position {
+    type Storage = VecStorage<Self>;
+}
+
+
+struct State {
+    ecs: World
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
@@ -12,6 +36,12 @@ impl GameState for State {
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike").build()?;
-    let gs = State {};
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    //look at the types and assign storage
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
     rltk::main_loop(context, gs)
 }
