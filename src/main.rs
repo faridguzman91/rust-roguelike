486ff33c25b9f85461aeee1815b0ef3c91f27bb8
Farid Guzman <farid.guzman91@hotmail.com>
2026-02-15 23:50:47 +0100

    added position and render item in main

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,73 @@
 use rltk::{GameState, RGB, Rltk};
 use specs::prelude::*;
-use std::cmp::{max, min};
 use specs_derive::Component;
+use std::cmp::{max, min};
 
 #[derive(Component)]
 struct Position {
     x: i32,
-    y: i32
+    y: i32,
 }
 
 #[derive(Component)]
 struct Renderable {
     glyph: rltk::FontCharType,
     fg: RGB,
-    bg: RGB
+    bg: RGB,
 }
 
-
-impl Component for Position {
-    type Storage = VecStorage<Self>;
-}
-
+// impl Component for Position {
+//     type Storage = VecStorage<Self>;
+// }
 
 struct State {
-    ecs: World
+    ecs: World,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello world")
+        //ask ecs for read-access to read-only containers
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike").build()?;
-    let mut gs = State {
-        ecs: World::new()
-    };
+    let mut gs = State { ecs: World::new() };
 
     //look at the types and assign storage
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40 , y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
+    // create entity with position and render with @ symbol in yellow on black
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('@'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
     rltk::main_loop(context, gs)
 }
