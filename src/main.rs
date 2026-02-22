30f89ab3afa34efa6b0d4593941a6ee55c981490
Farid Guzman <farid.guzman91@hotmail.com>
2026-02-23 00:04:33 +0100

    added matching for key inputs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, RGB, Rltk};
+use rltk::{GameState, RGB, Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
@@ -16,12 +16,17 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component, Debug)]
+struct Player{}
+
+
 #[derive(Component)]
 struct LeftMover {}
 
 struct State {
     ecs: World,
 }
+
 
 // impl Component for Position {
 //     type Storage = VecStorage<Self>;
@@ -37,6 +42,29 @@ impl GameState for State {
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+let mut positions = ecs.write_storage::<Position>();
+let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.y + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    } 
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {} //nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -72,6 +100,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -80,7 +109,7 @@ fn main() -> rltk::BError {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
-        })
+        }).with(Player{})
         .build();
 
     // create entity with position and render with @ symbol in yellow on black
