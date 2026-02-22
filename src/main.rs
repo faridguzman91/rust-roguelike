aec2fc209e02b617348f5670fb7cdc2223a42e0b
Farid Guzman <farid.guzman91@hotmail.com>
2026-02-22 23:46:42 +0100

    added LeftWalker impl lifetime Sytem and State system runner

--- src/main.rs
+++ src/main.rs
@@ -19,29 +19,18 @@ struct Renderable {
 #[derive(Component)]
 struct LeftMover {}
 
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79 ;}
-        }
-    }
+struct State {
+    ecs: World,
 }
 
 // impl Component for Position {
 //     type Storage = VecStorage<Self>;
 // }
 
-struct State {
-    ecs: World,
-}
-
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        self.run_systems();
         //ask ecs for read-access to read-only containers
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -49,6 +38,28 @@ impl GameState for State {
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+struct LeftWalker {}
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -70,7 +81,6 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
-        .with(LeftMover {})
         .build();
 
     // create entity with position and render with @ symbol in yellow on black
@@ -83,6 +93,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
 
