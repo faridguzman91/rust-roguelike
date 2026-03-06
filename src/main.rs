7a825a9cfe3f4f2eb1c2c3432fbbf2edbcfbeb4d
Farid Guzman <farid.guzman91@hotmail.com>
2026-03-07 00:22:09 +0100

    cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,21 @@
-use rltk::{GameState, RGB, Rltk, VirtualKeyCode};
+use rltk::{GameState, RGB, Rltk};
 use specs::prelude::*;
-use specs_derive::Component;
 
 mod map;
 pub use map::*;
 
+mod components;
+pub use components::*;
+
+mod player;
+use player::*;
+
 mod rect;
 pub use rect::*;
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(PartialEq, Copy, Clone)]
-enum TileType {
-    Wall,
-    Floor,
-}
-
-#[derive(Component)]
-struct LeftMover {}
 
 struct State {
     ecs: World,
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
 }
 
 fn new_map() -> Vec<TileType> {
@@ -70,88 +47,23 @@ fn new_map() -> Vec<TileType> {
 }
 
 
-
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-
-    for tile in map.iter() {
-        // render a tile depending upon the tile type
-        match tile {
-            TileType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.'),
-                );
-            }
-            TileType::Wall => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.0, 1.0, 0.0),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('#'),
-                );
-            }
-        }
-
-        // move the coordinates
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-// impl Component for Position {
-//     type Storage = VecStorage<Self>;
-// }
-
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        self.run_systems();
+
         player_input(self, ctx);
+        self.run_systems();
         //ask ecs for read-access to read-only containers
+        
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&*map, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        let map = self.ecs.fetch::<Vec<TileType>>();
+        
 
-        draw_map(&map, ctx);
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
-    }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-
-        if map[destination_idx] != TileType::Wall {
-            pos.x = (pos.x + delta_x).clamp(0, 79);
-            pos.y = (pos.y + delta_y).clamp(0, 49);
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {} //nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
     }
 }
 
