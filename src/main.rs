758bfaadc6437ef364bb78556248e4cbe9b302c0
Farid Guzman <farid.guzman91@hotmail.com>
2026-03-08 23:33:56 +0100

    main.rs map fix

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,7 @@ impl GameState for State {
         self.run_systems();
         //ask ecs for read-access to read-only containers
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&*map, ctx);
+        draw_map(&self.ecs, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -59,9 +58,9 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
 
-    let (rooms, map) = Map::new_map_rooms_and_corridors();
+    let map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
