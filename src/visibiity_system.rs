b07c4247f12e54b186215acd6ef3622b69148a80
Farid Guzman <farid.guzman91@hotmail.com>
2026-03-08 23:56:33 +0100

    check if an entity is the player

--- src/visibiity_system.rs
+++ src/visibiity_system.rs
@@ -1,4 +1,4 @@
-use super::{Map, Position, Viewshed};
+use super::{Map, Player, Position, Viewshed};
 use rltk::{Point, field_of_view};
 use specs::prelude::*;
 
@@ -6,21 +6,35 @@ pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
     type SystemData = (
+        //expect a map write , if not fail
+        WriteExpect<'a, Map>,
+        Entities<'a>,
         //expect a map read , if not fail
-        ReadExpect<'a, Map>,
         WriteStorage<'a, Viewshed>,
         WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (map, mut viewshed, pos) = data;
+        let (mut map, entities, mut viewshed, pos, player) = data;
         // clear visible tiles
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
             viewshed.visible_tiles.clear();
             //provides field of view from position starting point
             viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map); // deref and ref to unwrap Map from ecs
             //provide lambda to retain method to delete unmatched criteria entries
-            viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+            viewshed
+                .visible_tiles
+                .retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+
+            // if this is player, reveal their pov
+            let p: Option<&Player> = player.get(ent);
+            if let Some(_p) = p {
+                for vis in viewshed.visible_tiles.iter() {
+                    let idx = map.xy_idx(vis.x, vis.y);
+                    map.revealed_tiles[idx] = true
+                }
+            }
         }
     }
 }
